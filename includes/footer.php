84e8d691a3a06e35f4d1a939e0244a021fdc88d8
Namchai Laksap <94771083+WhitV@users.noreply.github.com>
2025-02-10 17:35:47 +0700

    updete

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 </div> <!-- End container -->
-    <footer class="text-white text-center py-3 mt-4">
-        <p>&copy; <?php echo date('Y'); ?> Asset Management System. All Rights Reserved.</p>
+    <footer class="text-center py-3 mt-4">
+        <p class="text-<?php echo $theme == 'dark' ? 'light' : 'dark'; ?>">&copy; <?php echo date('Y'); ?> Asset Management System. All Rights Reserved.</p>
     </footer>
 </body>
 </html>
